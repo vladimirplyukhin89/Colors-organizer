0b774e5c2980e259d97e67f6458b95b5fc10c261
Vladimir Plyukhin <vladimirplyukhin89@gmail.com>
2022-03-28 15:24:30 -1100

    hook useInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import { useState } from 'react';
 import colorData from './color-data.json';
 import ColorList from './components/ColorList';
+import AddColorForm from './components/AddColorForm';
 
 import './App.css';
 
 
 function App() {
   const [colors, setColors] = useState(colorData);
-  return <ColorList
-    colors={colors}
-    onRateColor={(id, rating) => {
-      const newColors = colors.map(color =>
-        color.id === id ? { ...color, rating } : color);
-      setColors(newColors);
-    }}
-    onRemoveColor={id => {
-      const newColors = colors.filter(color => color.id !== id);
-      setColors(newColors);
-    }}
-  />;
+
+
+  return (
+    <>
+      <ColorList
+        colors={colors}
+        onRateColor={(id, rating) => {
+          const newColors = colors.map(color =>
+            color.id === id ? { ...color, rating } : color);
+          setColors(newColors);
+        }}
+        onRemoveColor={id => {
+          const newColors = colors.filter(color => color.id !== id);
+          setColors(newColors);
+        }}
+      />;
+      <AddColorForm />
+    </>
+  )
 };
 
 export default App;
---
+++ src/components/AddColorForm.jsx
@@ -0,0 +1,31 @@
+import useInput from '../hooks/useInput';
+
+const AddColorForm = ({ onNewColor = f => f }) => {
+    const [titleProps, resetTitle] = useInput('');
+    const [colorProps, resetColor] = useInput('#000000');
+
+    const submit = e => {
+        e.preventDefault();
+
+        onNewColor(titleProps.value, colorProps.value);
+        resetTitle();
+        resetColor();
+    };
+
+    return (
+        <form onSubmit={submit}>
+            <input
+                {...titleProps}
+                type="text"
+                placeholder="color title..."
+                required />
+            <input
+                {...colorProps}
+                type="color"
+                required />
+            <button>ADD</button>
+        </form>
+    );
+};
+
+export default AddColorForm;
